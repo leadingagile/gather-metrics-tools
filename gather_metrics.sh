dce57290c28a09f6fef5fb3bec22ac52bbdfe6fd
Butch Howard <bhowar68@ford.com>
2022-02-23 15:16:17 -0500

    Default start date to today
    because we go BACKWARDS in time for N steps.

--- gather_metrics.sh
+++ gather_metrics.sh
@@ -1,4 +1,13 @@
- #!/usr/bin/env bash
+#!/usr/bin/env bash
+
+# Run the 'metrics.utility gather-multi' tool
+# Processes all of the repositories found in the PROJECTS_FOLDER to
+#   create a new run of data to be recorded as RUN_NAME
+#   beginning at START_DATE in each of the repostories
+
+# Be sure each repository is currently on the branch you are
+#   interested in evaluating
+
 
 # before using:
 #   (you probably need to be OFF the VPN to do this)
@@ -11,18 +20,18 @@
 
 
 RUN_NAME="${1:-"$(date -j +"%Y-%m-%d_%H-%M-%S%z")"}"
-START_DATE="${2:-2021-01-01}"
+START_DATE="${2:-"$(date -j +"%Y-%m-%d")"}"
 STEPS="${3:-52}"
 CONFIG_FILE="$4"
 PROJECTS_FOLDER="${5:-"$HOME/projects/ford_shopbuy"}"
 REPORTS_FOLDER="${6:-"$HOME/projects/reports"}"
 
  docker run -it --rm \
- -v "${PROJECTS_FOLDER}":/opt/repos \
--v "${REPORTS_FOLDER}":/opt/output \
- leadingagilestudios.azurecr.io/analysis/gather-cli:0.2.0 \
- -d "${START_DATE}" \
- -r "${RUN_NAME}" \
- -t "${STEPS}" \
- -c "${CONFIG_FILE}"
+    -v "${PROJECTS_FOLDER}":/opt/repos \
+    -v "${REPORTS_FOLDER}":/opt/output \
+    leadingagilestudios.azurecr.io/analysis/gather-cli:0.2.0 \
+    -d "${START_DATE}" \
+    -r "${RUN_NAME}" \
+    -t "${STEPS}" \
+    -c "${CONFIG_FILE}"
 
