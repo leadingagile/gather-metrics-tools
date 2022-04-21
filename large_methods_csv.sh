228ee83b506b052b4421533aad59bafd6f135c15
Butch Howard <bhowar68@ford.com>
2022-04-21 15:54:16 -0400

    Add parameter validations to script

--- large_methods_csv.sh
+++ large_methods_csv.sh
@@ -2,9 +2,11 @@
 
 # List the methods that are over the limit (mean+2*stdev)
 
-RUN_PATH="${1:-${HOME}/projects/reports/BUY_GUX_EU/2021_Annual}"
+RUN_PATH="${1}"
 LANGUAGE="${2:-"javascript"}"
 
+[[ -n "${RUN_PATH}" ]] || { echo "Full path to run folder required"; exit 1; }
+[[ -d "${RUN_PATH}/statistics" ]] || { echo "Run Path does not appear to have metrics data"; exit 1; }
 type -t jq > /dev/null 2>&1 || { echo "needs jq. Install with 'brew install jq"; exit 1; }
 
 LIMIT_LINES="$(jq '.data[0].method_lines.limit ' "${RUN_PATH}/statistics/${LANGUAGE}/counts_over_limit.json")"
